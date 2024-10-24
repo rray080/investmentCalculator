be1888df78497e8d3ec13a7e08ee4a4f85855e1f
Rakesh K Ray <rakeshrayk@gmail.com>
2024-10-24 12:49:42 +0530

    investment project
    investment project using a standalone

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject, Input } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  private investmentService = inject(InvestmentService);
+
+  //results = this.investment.resultsData().readOnly();
+  results = computed(() => this.investmentService.resultsData()); //this will be readonly data
+  // get results() {//direct data can be changed
+  //   return this.investmentService.resultsData;
+  // }
+  // @Input() results?: {
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[];
+}
---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+//import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  //@Output() calculate = new EventEmitter<InvestmentInput>();
+  //calculate = output<InvestmentInput>();
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  constructor(private investmentService: InvestmentService) {}
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+    });
+    // this.calculate.emit({
+    //   initialInvestment: +this.enteredInitialInvestment(),
+    //   duration: +this.enteredDuration(),
+    //   expectedReturn: +this.enteredExpectedReturn(),
+    //   annualInvestment: +this.enteredAnnualInvestment(),
+    // });
+  }
+}
